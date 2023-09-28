31d9079bd6bd8816a01354bcdde30acfd37ccbd0
Abdullah sajid <abdullahsajid521@gmail.com>
2023-09-29 00:03:31 +0500

    -update app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const dbConnect = require('./db/dbConnect')
 const cloudinary = require('cloudinary')
 
 const corsOptions = {
-    origin:'https://twitterclonefrontend.onrender.com/', 
+    origin:'https://twitterclonefrontend.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
